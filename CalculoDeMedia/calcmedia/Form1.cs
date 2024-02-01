af85ffa44b6dae150b996b3e2bcd6a98e9ffd230
Mateus Henrique <131814925+Code00MH@users.noreply.github.com>
2024-02-01 14:47:36 -0300

    Add files via upload

---
+++ CalculoDeMedia/calcmedia/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace calcmedia
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)//calcular
+        {
+            float soma, media, valor;
+            soma = 0;
+            foreach(Control controle in this.Controls)
+            {
+                if (controle is TextBox)
+                {
+                    valor = Convert.ToSingle((((TextBox)controle).Text));
+                    soma += valor;
+                    media = soma / 4;
+                    this.Controls["label7"].Text = media.ToString();
+                }
+            }
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e) //limpar valores
+        {
+            foreach (Control controle in this.Controls) 
+            {
+                if(controle is TextBox)
+                {
+                    ((TextBox)controle).Text = "";
+                }
+                this.Controls["label7"].Text = "...";
+            }
+
+        }
+    }
+}
